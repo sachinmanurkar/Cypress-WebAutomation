3c9ecf989b54ac036f03ca5417057aad0e3bd528
Sachin M <sachinm@Sachins-MacBook-Pro.local>
2020-05-21 11:34:44 +0530

    Added multiple environment support to run tests
    - Added qa,prod, staging json files in config folder
    - Refactored index.js file
    - Refactored package.json file

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -19,3 +19,18 @@ module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
 }
+
+const fs = require('fs-extra');
+const path = require('path');
+
+
+function getCongByFile(file) {
+  const pathToCongFile = path.resolve('cypress/', 'config', `${file}.json`);
+  return fs.readJson(pathToCongFile)
+}
+
+module.exports = (on, config) => {
+
+  const file = config.env.configFile;
+  return getCongByFile(file);
+}
